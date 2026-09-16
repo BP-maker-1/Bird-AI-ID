ef03457d97b02ed401d5f441db4847fa253b65c1
BP-maker-1 <potterbennett525@gmail.com>
2026-09-16 13:58:07 +1200

    Update Train.py

--- Train.py
+++ Train.py
@@ -1,10 +1,10 @@
 from ultralytics import settings, YOLO
 
 # Set datasets directory
-settings.update({"datasets_dir":   "path to data set"})
+settings.update({"datasets_dir":   "/Users/bennettpotter/Desktop/Coding/AI-Birds/Bird-AI-ID/datasets/train"})
 
 # Load model
-model = YOLO("path to best latest model")
+model = YOLO("/Users/bennettpotter/Desktop/Coding/AI-Birds/Bird-AI-ID/best.pt")
 
 # Train the model
 model.train(
@@ -12,9 +12,24 @@ model.train(
     imgsz=256,
     device="mps",
     verbose=False,
-    data="path to data set",
+    data="/Users/bennettpotter/Desktop/Coding/AI-Birds/Bird-AI-ID/datasets/nz_birds_dataset_split",
     patience=15
 )
+results = model.val(
+    imgsz=256,
+    device="mps",
+    verbose=False,
+    data="/Users/bennettpotter/Desktop/Coding/AI-Birds/Bird-AI-ID/datasets/nz_birds_dataset_split",)
+
+for result in results:
+    boxes = result.boxes      # None for classification
+    masks = result.masks      # None for classification
+    keypoints = result.keypoints  # None for classification
+    probs = result.probs      # Probs object containing classification predictions
+    obb = result.obb          # None for classification
+    
+    result.show()             # Display prediction on screen
+    result.save(filename="result.jpg")  # Save prediction output to disk
 
 # Process and display inference results
 for result in results:
